d18940038441693e04319b2ee874f4dad0781fe5
Tony Pham <phamton@os1.engr.oregonstate.edu>
2020-03-08 19:50:06 -0700

    final submission

--- client/ftclient.c
+++ client/ftclient.c
@@ -1,10 +1,11 @@
 /***********************************************************************************************
  * Author: Tony
  * Program: Chat Client
- * Description: This program allows a user to connect to a chat server. On execution, this 
- * program takes 2 parameters, host and port number.
+ * Description: This program allows a user to connect to an ftp server and either request the
+ * server's directory or request a specific file. This program takes 3 parameters for requesting
+ * the directory and 4 for requesting a specific file.
  * Course: CS372 Intro to Networks
- * Last Modified: 2/9/2020
+ * Last Modified: 3/8/2020
 ************************************************************************************************/
 
 
@@ -16,6 +17,8 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <time.h>
+#include <dirent.h>
 
 /************************************************************************************
  * This function prompts the user for a handle and appends '>' to it.
@@ -64,15 +67,16 @@ sendMessage(char* message, int socketFD, int size) {
  * The function will read a message into the buffer from the socket. If no data was read, an
  * error message will be displayed.
  * pre: pointer to char buffer, size of buffer, and int socket is passed to function
- * post: message from socket is read into the buffer. Nothing is returned
+ * post: message from socket is read into the buffer. Amount of bytes read is returned
 ***********************************************************************************************/
-readMessage(char* buffer, int socketFD, int size) {
+int readMessage(char* buffer, int socketFD, int size) {
 	int charsRead;
 	memset(buffer, '\0', size);
 	charsRead = recv(socketFD, buffer, size, 0);
 	if (charsRead < 0) {
 		fprintf(stderr, "CLIENT: ERROR reading from socket");
 	};
+	return charsRead;
 };
 
 /***********************************************************************************************
@@ -96,7 +100,7 @@ int checkForQuit(char* buffer) {
 int createSocket() {
 	int socketFD;
 	socketFD = socket(AF_INET, SOCK_STREAM, 0);
-	if (socketFD < 0) fprintf(stderr, "CLIENT: ERROR CONNECTING\n");
+	if (socketFD < 0) fprintf(stderr, "CLIENT: ERROR CREATING SOCKET\n");
 	return socketFD;
 };
 
@@ -108,7 +112,7 @@ int createSocket() {
 //will establish connection between the socket and the address.
 connectSocket(struct sockaddr_in serverAddress, int socketFD, int size) {
 	if (connect(socketFD, (struct sockaddr*)&serverAddress, size) < 0) {
-		fprintf(stderr, "CLIENT: ERROR CONNECTING\n");
+		fprintf(stderr, "CLIENT: ERROR CONNECTING TO SOCKET\n");
 	};
 };
 
@@ -122,25 +126,93 @@ terminateConnection(int *isDone) {
 	*isDone = 1;
 };
 
+/******************************************************************************************************
+ * output 1 if number of commandline arguments does not equal 5 or 6 (including prgram name)
+ * pre: program is executed and argc is passed to function
+ * post: returns 1 if valid 0 otherwise
+******************************************************************************************************/
+int checkArgumentsCount(int count) {
+	if (count == 5 || count ==6) {
+		return 1;
+	} else {
+		printf("Invalid count of arguments\n");
+		exit(1);
+	};
+};
+
+/*****************************************************************************************************
+ * checks current directory for a file with the same name
+ * pre: pass the name of the file to be checked
+ * post: returns 0 if no file found or 1 if file found
+*****************************************************************************************************/
+int checkForFile(char* fileName) {
+	struct dirent *fileInDir;
+	DIR* dirToCheck = opendir(".");
+
+	if (dirToCheck > 0) {
+		while ((fileInDir = readdir(dirToCheck)) != NULL) {
+			if (strcmp(fileName, fileInDir->d_name) == 0) {
+				return 1;
+			};
+		};
+
+	return 0;
+	};
+};
+
+/*****************************************************************************************************
+ * Appends -copy to file name
+ * pre: pass file name to have -copy appended
+ * post: pointer to string 
+*****************************************************************************************************/
+void appendCopy(char* string, int length) {
+	char* copy = "-copy.txt";
+	strcat(string + length, copy); 
+};
+
 int main(int argc, char*argv[]) {
-	int socketFD, portNumber, charsRead;
+	int socketFD, portNumber, charsRead, socketFDData, portNumberData;
 	struct sockaddr_in serverAddress;
+	struct sockaddr_in serverAddressData;
 	struct hostent* serverHostInfo;
+	struct hostent* serverHostInfoData;
 	char buffer[100000];
 	char userHandle[1000];
-	char greeting[11] = "hello world";
 	int isDone = 0;
 	
-	//printf("arg 1 is: %s", argv[0]);
-	//printf("arg 2 is: %s", argv[1]);
-	//printf("arg 3 is: %s", argv[2]);
-	//printf("arg 4 is: %s", argv[3]);
-	//exit(0);
+	char temp[1000];
+
+	//Validate parameters
+	checkArgumentsCount(argc);
+
+	//Initialize commandline parameters
+	char fileName[10000];
+	char command[200];
+	char request[1000];
+
+	memset(fileName, '\0', sizeof(fileName));
+	memset(command, '\0', sizeof(command));
+	memset(request, '\0', sizeof(request));
+
+	strcpy(command, argv[3]);
+	if (strcmp(command, "-g") == 0) {
+		strcpy(fileName, argv[4]);
+	};
+
+	//prep initial message
+	memset(request, '\0', sizeof(request));
+	strcpy(request, argv[3]);
+	request[strlen(request)] = ' ';
+	strcat(request, argv[4]);
+	if (argc > 5) {
+		request[strlen(request)] = ' ';
+		strcat(request, argv[5]);
+	};
 
 	//Get handle from user
 	getHandleFromUser(userHandle);
 
-	//set up the server address struct
+	//set up the server address struct for control connection
 	memset((char*)&serverAddress, '\0', sizeof(serverAddress)); //Clear out the address struct
 	portNumber = atoi(argv[2]); //Get the port number, convert to an integer from a string
 	serverAddress.sin_family = AF_INET; //Create a network-capable socket
@@ -152,37 +224,52 @@ int main(int argc, char*argv[]) {
 	memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length);
 
 	//Set up the socket
-	//socketFD = socket(AF_INET, SOCK_STREAM, 0); //Create a TCP socket
-	//if (socketFD < 0) fprintf(stderr, "CLIENT: ERROR CONNECTING\n");
 	socketFD = createSocket();
-
-	//Connect to server
-	//if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
-	//	fprintf(stderr, "CLIENT: ERROR CONNECTING\n");
-	//};
 	connectSocket(serverAddress, socketFD, sizeof(serverAddress));
 
-	//Send greeting server
-	sendMessage(greeting, socketFD, strlen(greeting));
+	//Send command to server
+	sendMessage(request, socketFD, strlen(request));
+	readMessage(buffer, socketFD, sizeof(buffer));
 
-	//Loop through receiving and sending message until \quit is received from server or if user inputs
-	while (!isDone) {
-		readMessage(buffer, socketFD, sizeof(buffer));
-		if (checkForQuit(buffer))  {
-			terminateConnection(&isDone);
+	sleep(5);
+	//set up the server address struct for data connection
+	memset((char*)&serverAddressData, '\0', sizeof(serverAddressData)); //Clear out the address struct
+	portNumberData = atoi(argv[argc - 1]); //Get the port number for data connection, convert to an integer from a string
+	
+	//Connect to server with new port
+	serverAddressData.sin_family = AF_INET;
+	serverAddressData.sin_port = htons(portNumberData); //Convert port number to network type
+	serverHostInfoData = gethostbyname(argv[1]);
+	memcpy((char*)&serverAddressData.sin_addr.s_addr, (char*)serverHostInfoData->h_addr, serverHostInfoData->h_length);
+
+
+	socketFDData = createSocket();
+	connectSocket(serverAddressData, socketFDData, sizeof(serverAddressData));
+
+	int messageSize;
+	int count = 0;
+	readMessage(buffer, socketFDData, sizeof(buffer));
+
+	close(socketFD);
+
+	if (strcmp(command, "-l") == 0) {
+		printf("Receiving directory structure from server on port %d\n", portNumberData);
+		printf("%s", buffer);
+	}
+	else if (strcmp(command, "-g") == 0) {
+		if (strcmp(buffer, "File Not Found") == 0) {
+			printf("%s\n", buffer);
 		}
 		else {
-			printf("Server: %s\n", buffer);
-			//Get next message from user
-			memset(buffer, '\0', sizeof(buffer));
-			printf("%s ", userHandle);
-			fgets(buffer, sizeof(buffer), stdin);
-			buffer[strcspn(buffer, "\n")] = '\0';
-			sendMessage(buffer, socketFD, strlen(buffer));
-			if (checkForQuit(buffer)) {
-				terminateConnection(&isDone);
+			printf("Receiving %s from server\n", fileName);
+			while (checkForFile(fileName)) {
+				appendCopy(fileName, strlen(fileName));
 			};
-		};
+			FILE* outputFilePtr = fopen(fileName, "w");
+			fprintf(outputFilePtr, buffer);
+			fclose(outputFilePtr);
+			printf("File transfer complete\n");
+		}		
 	};
 
 	return 0;
--- server/ftserver.py
+++ server/ftserver.py
@@ -2,13 +2,13 @@
 #Author: Tony Pham
 #Program: Chat Server
 #Course: CS372
-#Description: This program acts as server for a chat system. On execution, the program takes
-#one parameter as the port number. Clients that connect to this server will take turns sending
-#and receiving mesages.
-#Last Modified: 2/9/2020
+#Description: This program acts as server for an ftp server. On execution, this program takes one
+#paremeter, the port number to run on.
+#Last Modified: 3/8/2020
 #################################################################################################
 import socket
 import sys
+import os
 
 #getMessage will call recv and wait for client to respond. The response is decoded and returned
 #pre: socket is created
@@ -41,6 +41,32 @@ def checkForQuit(input):
 def terminateConnection():
 	print("Connection terminated")
 
+#Retrieve list of files based on path and extension provided
+#ore: N/A
+#post: returns a string of file names
+def retrieveFiles(path, extension=False):
+	fileList = ""
+	if extension:
+		for filename in os.listdir(path):
+			if filename.endswith(extension):
+				fileList += filename
+				fileList += '\n'
+	else:
+		for filename in os.listdir(path):
+			fileList += filename
+			fileList += '\n'
+	return fileList
+
+#Searches through directory for file name. Returns true if found
+#pre: N/A
+#post: returns true if file found, false otherwise
+def searchForFile(path, file):
+	for filename in os.listdir(path):
+		if filename == file:
+			return True
+	return False
+
+
 if len(sys.argv) < 2:
     print("Please pass port number as command line argument 1")
     exit(0)
@@ -48,13 +74,12 @@ if len(sys.argv) < 2:
 serverHost = 'localhost' #is this right?
 serverPort = int(sys.argv[1])
 
-
-
 #Title: Socket Programming in Python source code
 #Author: Nathan Jennings
 #Date: 2/4/2020
 #Code Version: Version: 1
 #Availability: realpython.com/python-sockets/
+print('Server open on ' + str(serverPort))
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket: 
 	serverSocket.bind((serverHost, serverPort))
 	while True:
@@ -63,23 +88,40 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
 		with conn:
 			print('Connected by', addr)
 			isDone = False
-			while not isDone:
-				data = getMessage()	
-				print("message is" + data)
-				if checkForQuit(data):
-					terminateConnection()
-					isDone = True
-				elif data == "-l":
-					reply = "Here is my directory"
-					conn.sendall(reply.encode("utf-8"))
-				elif data == "-g":
-					reply = "Let me check if we have that file"
-					conn.sendall(reply.encode("utf-8"))
-					#reply = sendMessage()
-#				elif checkForQuit(reply):
-#					terminateConnection()
-#					isDone = True
-				else:
-					reply = "Command not recognized"
-					conn.sendall(reply.encode("utf-8"))
-	
+			data = getMessage()
+			data = data.split(' ')
+			command = data[0]
+			fileName = data[1]
+			serverPortData = int(data[-1])
+			#Validate response
+			if data[0] == "-l" or data[0] == "-g":
+				reply = ("Establishing data connection on port " + str(serverPortData))
+				conn.sendall(reply.encode("utf-8"))
+				
+				serverPortData = data[-1];
+				with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocketData:
+					serverSocketData.bind((serverHost, int(serverPortData)))
+					serverSocketData.listen()
+					connData, addrData = serverSocketData.accept()
+					with connData:
+						if data[0] == "-l":
+							print("List directory requested on port " + str(serverPortData))
+							reply = retrieveFiles('.', '.txt')
+							print("Sending directory contents\n")
+							connData.sendall(reply.encode("utf-8"))
+							connData.close()
+						else:
+							print("File " + fileName + " requested\n")
+							if searchForFile('.', data[1]):
+								inputFile = open(data[1], "r")
+								reply = inputFile.read()
+								print("Sending " + fileName)
+								connData.sendall(reply.encode("utf-8"))
+								connData.close()
+							else:
+								reply = "File Not Found"
+								connData.sendall(reply.encode("utf-8"))
+								connData.close()
+
+			else:
+				print("invalid")
